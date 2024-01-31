79ddd93cbe3bf666fc15f9909b9b8cf1ccb298c1
asyarifstudio <asyarif.studio@gmail.com>
2024-01-31 11:52:46 +0800

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css"
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+ 
+    inject({ mode: dev ? 'development' : 'production' });   
 </script>
 <div class="h-screen w-screen flex">
     <!-- menu -->
